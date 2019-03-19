ea2b0a0d47de2cd70cf8687a98157ea7abd690ec
Mathieu Lecarme <mlecarme@bearstech.com>
2019-03-19 14:46:01 +0100

    Fix: shellcheck.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "${PASSWORD:-nope}" == "nope" ]; then
     echo "PASSWORD env is mandatory" > /dev/stderr
     exit 1
 fi
-neo4j-admin set-initial-password $PASSWORD
+neo4j-admin set-initial-password "$PASSWORD"
 
 echo "$@"
 exec "$@"
